a4d3c248d1ba66553162b95b6c4c29b71f3e0f16
josefrancisco <josefrancisco281101@gmail.com>
2024-08-14 23:42:50 -0400

    revision final resalizada

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,21 +2,19 @@ import eggImgMobile from "../assets/images/mobile/image-transform.jpg";
 import eggImgDesktop from "../assets/images/desktop/image-transform.jpg";
 import glassImgMobile from "../assets/images/mobile/image-stand-out.jpg";
 import glassImgDesktop from "../assets/images/desktop/image-stand-out.jpg";
-import graphicDesingDesktop from "../assets/images/desktop/image-graphic-design.jpg"
-import graphicDesingMobile from "../assets/images/mobile/image-graphic-design.jpg"
-import photographyDesktop from "../assets/images/desktop/image-photography.jpg"
-import photographyMobile from "../assets/images/mobile/image-photography.jpg"
+import graphicDesingDesktop from "../assets/images/desktop/image-graphic-design.jpg";
+import graphicDesingMobile from "../assets/images/mobile/image-graphic-design.jpg";
+import photographyDesktop from "../assets/images/desktop/image-photography.jpg";
+import photographyMobile from "../assets/images/mobile/image-photography.jpg";
 
 import { Image } from "./Image";
 import { Text } from "./Text";
 import { ImageText } from "./ImageText";
 
-
 export const About = () => {
   return (
     <section>
       <div className="tablet:grid tablet:grid-cols-2">
-
         <Image
           order="order-2"
           imgMobile={eggImgMobile}
@@ -25,11 +23,11 @@ export const About = () => {
 
         <Text
           order="order-1"
-          title='Transform your brand'
-          text='We are a full-service creative agency specializing in helping brands
+          title="Transform your brand"
+          text="We are a full-service creative agency specializing in helping brands
           grow fast. Engage your clients through compelling visuals that do
-          most of the marketing for you.'
-          color='bg-Yellow'
+          most of the marketing for you."
+          color="bg-Yellow"
         />
 
         <Image
@@ -40,25 +38,25 @@ export const About = () => {
 
         <Text
           order="order-4"
-          title='Stand out to the right audience'
-          text='Using a collaborative formula of designers, researchers,
+          title="Stand out to the right audience"
+          text="Using a collaborative formula of designers, researchers,
           photographers, videographers, and copywriters, we’ll build and
-          extend your brand in digital places.'
-          color='bg-Soft-red'
+          extend your brand in digital places."
+          color="bg-Soft-red"
         />
 
         <ImageText
           imgMobile={graphicDesingMobile}
           imgDesktop={graphicDesingDesktop}
-          title='Graphic design'
-          text='Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.'
+          title="Graphic design"
+          text="Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention."
         />
 
         <ImageText
           imgMobile={photographyMobile}
           imgDesktop={photographyDesktop}
-          title='Photography'
-          text='Increase your credibility by getting the most stunning, high-quality photos that improve your business image.'
+          title="Photography"
+          text="Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
         />
       </div>
     </section>
